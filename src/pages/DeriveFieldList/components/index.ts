784fab684886f016f7c9bbfe713eb4d3e92505f0
zoucaihui <zoucaihui@qq.com>
2025-10-24 20:31:25 +0800

    fix: 删除不需要的衍生字段form与modal

--- src/pages/DeriveFieldList/components/index.ts
+++ src/pages/DeriveFieldList/components/index.ts
@@ -2,8 +2,5 @@
  * DeriveFieldList 页面组件导出
  */
 
-export { default as DeriveFieldForm } from './DeriveFieldForm';
 export { default as DeriveFieldTable } from './DeriveFieldTable';
-export { default as DeriveFieldModal } from './DeriveFieldModal';
 export { default as DeriveFieldViewModal } from './DeriveFieldViewModal';
-
