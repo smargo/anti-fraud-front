849c309f072c646abcf5fe15f2e628ed94cea579
zoucaihui <zoucaihui@qq.com>
2025-10-24 11:23:29 +0800

    fix: 删除不需要的代码

--- src/services/antifraud/statementDependency.ts
+++ src/services/antifraud/statementDependency.ts
@@ -1,3 +1,4 @@
+import { ResultPage } from '@/pages/EventConfigNew/types';
 import { request } from '@umijs/max';
 
 // 语句依赖接口
@@ -25,47 +26,12 @@ export interface StatementDependencyWithNamesItem {
   lastModifiedBy: string;
 }
 
-// 查询语句依赖
-export async function queryStatementDependencies(params: any) {
-  return request<StatementDependencyItem[]>('/api/statement-dependency/list', {
-    method: 'GET',
-    params,
-  });
-}
-
 // 查询语句依赖（包含语句描述）
-export async function queryStatementDependenciesWithNames(params: any) {
-  return request<StatementDependencyWithNamesItem[]>('/api/statement-dependency/list-with-names', {
+export async function queryStatementDependenciesWithNames(
+  params: any,
+): Promise<ResultPage<StatementDependencyWithNamesItem>> {
+  return request('/api/statement-dependency/list-with-names', {
     method: 'GET',
     params,
   });
-}
-
-// 创建语句依赖
-export async function createStatementDependency(data: StatementDependencyItem) {
-  const response = await request('/api/statement-dependency', {
-    method: 'POST',
-    data,
-  });
-
-  return response;
-}
-
-// 更新语句依赖
-export async function updateStatementDependency(data: StatementDependencyItem) {
-  const response = await request(`/api/statement-dependency/${data.id}`, {
-    method: 'PUT',
-    data,
-  });
-
-  return response;
-}
-
-// 删除语句依赖
-export async function deleteStatementDependency(id: string) {
-  const response = await request(`/api/statement-dependency/${id}`, {
-    method: 'DELETE',
-  });
-
-  return response;
 }
