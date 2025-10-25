ff7c20f5d373d12dbcc930dd1cafd83abf5c11a9
zoucaihui <zoucaihui@qq.com>
2025-10-25 14:34:02 +0800

    fix: 整理字段实体

--- src/services/antifraud/field.ts
+++ src/services/antifraud/field.ts
@@ -1,7 +1,9 @@
+import { FieldItem } from '@/pages/FieldList/types';
+import { ResultPage } from '@/types';
 import { request } from '@umijs/max';
 
 // 查询事件字段列表
-export async function queryEventFields(params?: any) {
+export async function queryEventFields(params?: any): Promise<ResultPage<FieldItem>> {
   const response = await request('/api/eventFields/list', {
     method: 'GET',
     params,
