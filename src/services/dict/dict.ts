6e142f9f77e87b69a39e4964388c1e53436c2508
zoucaihui <zoucaihui@qq.com>
2025-10-25 16:39:49 +0800

    fix: 整理字典实体类

--- src/services/dict/dict.ts
+++ src/services/dict/dict.ts
@@ -1,7 +1,10 @@
+import { SystemDictItem } from '@/pages/SystemDictList/types';
+import { ApiResponse, ResultPage } from '@/types';
+import { DictDataMap } from '@/types/dict';
 import { request } from '@umijs/max';
 
 // 查询系统字典列表
-export async function queryDicts(params?: any) {
+export async function queryDicts(params?: any): Promise<ResultPage<SystemDictItem>> {
   return await request('/api/dict/list', {
     method: 'GET',
     params,
@@ -9,7 +12,7 @@ export async function queryDicts(params?: any) {
 }
 
 // 创建系统字典
-export async function createDict(data: any) {
+export async function createDict(data: any): Promise<ApiResponse<boolean>> {
   return await request('/api/dict', {
     method: 'POST',
     data,
@@ -17,7 +20,7 @@ export async function createDict(data: any) {
 }
 
 // 更新系统字典
-export async function updateDict(data: any) {
+export async function updateDict(data: any): Promise<ApiResponse<boolean>> {
   return await request(`/api/dict/${data.id}`, {
     method: 'PUT',
     data,
@@ -25,21 +28,23 @@ export async function updateDict(data: any) {
 }
 
 // 删除系统字典
-export async function deleteDict(params: { dictId: number }) {
+export async function deleteDict(params: { dictId: number }): Promise<ApiResponse<boolean>> {
   return await request(`/api/dict/${params.dictId}`, {
     method: 'DELETE',
   });
 }
 
 // 根据代码编号查询字典列表
-export async function getDictsByCodeNo(codeNo: string) {
+export async function getDictsByCodeNo(codeNo: string): Promise<ApiResponse<SystemDictItem[]>> {
   return await request(`/api/dict/code/${codeNo}`, {
     method: 'GET',
   });
 }
 
 // 批量查询字典
-export async function getDictsByCodeNoList(codeNoList: string[]) {
+export async function getDictsByCodeNoList(
+  codeNoList: string[],
+): Promise<ApiResponse<DictDataMap>> {
   return await request('/api/dict/batch', {
     method: 'POST',
     data: codeNoList,
@@ -47,7 +52,7 @@ export async function getDictsByCodeNoList(codeNoList: string[]) {
 }
 
 // 刷新字典数据
-export async function refreshDicts() {
+export async function refreshDicts(): Promise<ApiResponse<string>> {
   return await request('/api/dict/refresh', {
     method: 'POST',
   });
